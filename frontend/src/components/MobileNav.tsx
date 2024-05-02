32e319b0f193b43704f17632587c7409d0e181c0
radegast0 <fatih.yonucuoglu@gmail.com>
2024-05-02 15:47:15 +0300

    nav login

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -8,8 +8,11 @@ import {
 } from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 export default function MobileNav() {
+  const { isAuthenticated, loginWithRedirect } = useAuth0();
   return (
     <Sheet>
       <SheetTrigger>
@@ -20,8 +23,17 @@ export default function MobileNav() {
           <span>Welcome to MernEats.com</span>
         </SheetTitle>
         <Separator />
-        <SheetDescription className="flex">
-          <Button className="flex-1 bg-orange-500 font-bold">Log In</Button>
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobileNavLinks />
+          ) : (
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 bg-orange-500 font-bold"
+            >
+              Log In
+            </Button>
+          )}
         </SheetDescription>
       </SheetContent>
     </Sheet>
