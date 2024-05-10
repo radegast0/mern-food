0ba9638663fb89f609ea0b2938042f400018f751
radegast0 <fatih.yonucuoglu@gmail.com>
2024-05-10 18:55:16 +0300

    canvas as hero

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
           to="/"
           className=" text-3xl font-bold tracking-tight text-purple"
         >
-          MernEats.com
+          MERN-R3Feats
         </Link>
         <div className="md:hidden">
           <MobileNav />
--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,9 +1,30 @@
-import hero from "../assets/hero.png";
+import { Canvas } from "@react-three/fiber";
+import {
+  PresentationControls,
+  Stage,
+} from "@react-three/drei";
+import Burger from "./Burger";
 
 export default function Hero() {
   return (
-    <div>
-      <img src={hero} className="max-h-[600px] w-full object-cover" />
+    <div className="flex h-72 w-full touch-none bg-purple sm:h-96 md:h-[486px] lg:h-[642px]">
+      <Canvas
+        shadows
+        camera={{
+          fov: 45,
+          position: [0, 0.5, 12],
+        }}
+      >
+        <Stage intensity={1.5} preset="soft">
+          <PresentationControls
+            global
+            snap
+            polar={[-Math.PI / 15, Math.PI / 36]}
+          >
+            <Burger />
+          </PresentationControls>
+        </Stage>
+      </Canvas>
     </div>
   );
 }
